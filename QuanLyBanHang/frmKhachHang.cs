fdb54742e7cc5aa3ed723891a8872b7429bec17a
unknown <maitrongnhan.001@gmail.com>
2021-11-01 11:22:00 +0700

    fix manager customer

--- QuanLyBanHang/frmKhachHang.cs
+++ QuanLyBanHang/frmKhachHang.cs
@@ -236,8 +236,8 @@ namespace QuanLyBanHang
                     cmd.CommandType = CommandType.Text;
                     //Lệnh Insert InTo
                     cmd.CommandText = System.String.Concat("Insert into KhachHang values(" + "'" + 
-                        this.txtMaKH.Text.ToString() + "','" + this.txtTenCty.Text.ToString() + "','" +
-                        this.txtDiachi.Text.ToString() + "','" + this.cbThanhPho.SelectedValue.ToString() + "','" +
+                        this.txtMaKH.Text.ToString() + "',N'" + this.txtTenCty.Text.ToString() + "',N'" +
+                        this.txtDiachi.Text.ToString() + "',N'" + this.cbThanhPho.SelectedValue.ToString() + "','" +
                         this.txtDienthoai.Text.ToString() + "')");
                     cmd.CommandType = CommandType.Text;
                     cmd.ExecuteNonQuery();
@@ -266,9 +266,9 @@ namespace QuanLyBanHang
                     //MaKH hiện hành
                     string strMAKH = dgvKhachHang.Rows[r].Cells[0].Value.ToString();
                     //Câu lệnh SQL
-                    cmd.CommandText = System.String.Concat("Update KhachHang Set TenCty='"+
-                        this.txtTenCty.Text.ToString() + "', Diachi ='" + 
-                        this.txtDiachi.Text.ToString() + "', ThanhPho ='" + this.cbThanhPho.SelectedValue.ToString()
+                    cmd.CommandText = System.String.Concat("Update KhachHang Set TenCty=N'"+
+                        this.txtTenCty.Text.ToString() + "', Diachi =N'" + 
+                        this.txtDiachi.Text.ToString() + "', ThanhPho =N'" + this.cbThanhPho.SelectedValue.ToString()
                         + "', DienThoai ='" + this.txtDienthoai.Text.ToString() + "', MaKH ='" + this.txtMaKH.Text.ToString() + 
                         "' where MaKH ='" + strMAKH + "'");
                     //Cập nhật
